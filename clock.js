3de07df13971ca29fb09ed8edd280d0f73bf7b41
Devanand Sapkale <kolideva54@gmail.com>
2024-02-28 23:13:55 +0530

    DIGITAL CLOCK PROJECT
    This program language are HTML, CSS, JS.
    this is a basic project for the coders.

--- clock.js
+++ clock.js
@@ -1,4 +1,13 @@
-function num(a,b){
-    console.log(a+b);
-}
-num(5,5);
+let hrs = document.getElementById("h1");
+let min = document.getElementById("m1");
+let sec = document.getElementById("s1");
+
+setInterval(()=>{
+    let currentTime = new Date();
+
+hrs.innerHTML =(currentTime.getHours()<10?"0":"")+currentTime.getHours();
+min.innerHTML =(currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes();
+sec.innerHTML =(currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
+
+},1000)
+
